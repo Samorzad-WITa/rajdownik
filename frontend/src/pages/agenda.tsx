5c96f5f9ca18bebd90992f9a3ef1dc50deefc671
Michał Korczak <michal.k2001@gmail.com>
2024-03-26 03:03:16 +0100

    feat: day picker buttons

--- frontend/src/pages/agenda.tsx
+++ frontend/src/pages/agenda.tsx
@@ -1,5 +1,6 @@
 import { Calendar } from '@/features/agenda/Calendar';
-import { Box, Button, Text, VStack, chakra } from '@chakra-ui/react';
+import { Box, Button, HStack, Icon, Text, VStack } from '@chakra-ui/react';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 import Head from 'next/head';
 
 export default function Agenda() {
@@ -12,23 +13,45 @@ export default function Agenda() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <VStack mt={3} gap={5}>
-        <Box
-          p={4}
-          paddingX={6}
-          bgColor="#ff1c37"
-          borderRadius={15}
-          display="flex"
-          flexDir="column"
-          alignItems="center"
-        >
-          <Text as="b" fontSize="large">
-            Sobota
-          </Text>
-          <br />
-          <Text as="b" fontSize="x-small">
-            13 kwietnia 2024
-          </Text>
-        </Box>
+        <HStack gap={2}>
+          <Button
+            size='xs'
+            pl={0}
+            variant="outline"
+            color="#283d4e"
+            outlineColor="#283d4e"
+            leftIcon={<Icon color="#283d4e" fontSize={30} as={ChevronLeft} />}
+          >
+            Piątek
+          </Button>
+          <Box
+            p={4}
+            paddingX={6}
+            bgColor="#ff1c37"
+            borderRadius={15}
+            display="flex"
+            flexDir="column"
+            alignItems="center"
+          >
+            <Text as="b" fontSize="large">
+              Sobota
+            </Text>
+            <br />
+            <Text as="b" fontSize="xx-small">
+              13 kwietnia 2024
+            </Text>
+          </Box>
+          <Button
+            size='xs'
+            pr={0}
+            variant="outline"
+            color="#283d4e"
+            outlineColor="#283d4e"
+            rightIcon={<Icon color="#283d4e" fontSize={30} as={ChevronRight} />}
+          >
+            Niedziela
+          </Button>
+        </HStack>
         <Calendar />
       </VStack>
     </>
