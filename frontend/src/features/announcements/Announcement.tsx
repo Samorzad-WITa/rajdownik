b51d2a42f3d62be0554b83f2072763be6af2b8fd
Michał Korczak <michal.k2001@gmail.com>
2024-03-31 04:24:39 +0200

    fix: announcement

--- frontend/src/features/announcements/Announcement.tsx
+++ frontend/src/features/announcements/Announcement.tsx
@@ -8,23 +8,27 @@ import {
   Text,
 } from '@chakra-ui/react';
 
-export const Announcement = (props: {
-  announcement: {
-    id: number;
-    title: string;
-    description: string;
-  };
+export type AnnouncementItem = {
+  id: number;
+  title: string;
+  description: string;
+};
+
+export const Announcement = ({
+  announcement,
+}: {
+  announcement: AnnouncementItem;
 }) => {
   return (
     <Card width="90%" backgroundColor="#344756" borderRadius={20} px={5}>
       <CardHeader p={3}>
         <Flex justify="center">
-          <Heading size="md">{props.announcement.title}</Heading>
+          <Heading size="md">{announcement.title}</Heading>
         </Flex>
       </CardHeader>
       <Divider />
       <CardBody px={0} pt={3}>
-        <Text>{props.announcement.description}</Text>
+        <Text>{announcement.description}</Text>
       </CardBody>
     </Card>
   );
--- frontend/src/pages/announcement/[id].tsx
+++ frontend/src/pages/announcement/[id].tsx
@@ -1,11 +1,17 @@
 import { OutlineButton } from '@/components/OutlineButton';
-import { Announcement } from '@/features/announcements/Announcement';
+import {
+  Announcement,
+  AnnouncementItem,
+} from '@/features/announcements/Announcement';
 import { Button, Flex, Icon, VStack } from '@chakra-ui/react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-export default function Home() {
+export default function Home({
+  items,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
 
   const announcement = items.find(
@@ -53,41 +59,47 @@ export default function Home() {
   );
 }
 
-const items = [
-  {
-    id: 7,
-    title: 'Ogłoszenie 7',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae convallis ante. Donec eget metus in nisl vestibulum pulvinar. In convallis cursus augue nec pretium. Sed blandit mi ex, sed vestibulum tortor ullamcorper ut. Maecenas ullamcorper vehicula accumsan. Donec eu mi id neque imperdiet sollicitudin. Pellentesque porta ornare velit in maximus. Proin posuere lectus sit amet posuere suscipit. ',
-  },
-  {
-    id: 6,
-    title: 'Ogłoszenie 6',
-    description: 'Opis ogłoszenia 6',
-  },
-  {
-    id: 5,
-    title: 'Ogłoszenie 5',
-    description: 'Opis ogłoszenia 5',
-  },
-  {
-    id: 4,
-    title: 'Ogłoszenie 4',
-    description: 'Opis ogłoszenia 4',
-  },
-  {
-    id: 3,
-    title: 'Ogłoszenie 3',
-    description: 'Opis ogłoszenia 3',
-  },
-  {
-    id: 2,
-    title: 'Ogłoszenie 2',
-    description: 'Opis ogłoszenia 2',
-  },
-  {
-    id: 1,
-    title: 'Ogłoszenie 1',
-    description: 'Opis ogłoszenia 1',
-  },
-];
+export const getServerSideProps = (async () => {
+  return {
+    props: {
+      items: [
+        {
+          id: 7,
+          title: 'Ogłoszenie 7',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae convallis ante. Donec eget metus in nisl vestibulum pulvinar. In convallis cursus augue nec pretium. Sed blandit mi ex, sed vestibulum tortor ullamcorper ut. Maecenas ullamcorper vehicula accumsan. Donec eu mi id neque imperdiet sollicitudin. Pellentesque porta ornare velit in maximus. Proin posuere lectus sit amet posuere suscipit. ',
+        },
+        {
+          id: 6,
+          title: 'Ogłoszenie 6',
+          description: 'Opis ogłoszenia 6',
+        },
+        {
+          id: 5,
+          title: 'Ogłoszenie 5',
+          description: 'Opis ogłoszenia 5',
+        },
+        {
+          id: 4,
+          title: 'Ogłoszenie 4',
+          description: 'Opis ogłoszenia 4',
+        },
+        {
+          id: 3,
+          title: 'Ogłoszenie 3',
+          description: 'Opis ogłoszenia 3',
+        },
+        {
+          id: 2,
+          title: 'Ogłoszenie 2',
+          description: 'Opis ogłoszenia 2',
+        },
+        {
+          id: 1,
+          title: 'Ogłoszenie 1',
+          description: 'Opis ogłoszenia 1',
+        },
+      ],
+    },
+  };
+}) satisfies GetServerSideProps<{ items: AnnouncementItem[] }>;
