3be242397091434a9bf8cad2b62238b79b371be2
Michał Korczak <michal.k2001@gmail.com>
2024-04-12 04:06:58 +0200

    fix: java?

--- backend/client-app/src/main/java/pl/pwr/ite/server/client/security/JwtAuthorizationFilter.java
+++ backend/client-app/src/main/java/pl/pwr/ite/server/client/security/JwtAuthorizationFilter.java
@@ -11,7 +11,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import pl.pwr.ite.server.security.AuthenticatedUser;
