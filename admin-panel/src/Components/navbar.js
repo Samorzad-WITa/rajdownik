13f1dc9950a48c1007d6904af11558461151d64e
HETTski <tanski.hubo@gmail.com>
2024-09-04 18:18:59 +0200

    updated layout, working on schedule

--- admin-panel/src/Components/navbar.js
+++ admin-panel/src/Components/navbar.js
@@ -1,12 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import participants from "./assets/icons/participants.svg"
+import announcements from "./assets/icons/anouncements.svg"
+import schedule from "./assets/icons/schedule.svg"
+import staff from "./assets/icons/staff.svg"
+import setting from "./assets/icons/settings.svg"
 
 function Navbar(){
+
+ const [token, setToken] = useState(null);
+    const [user, setUser] = useState([]);
+
+    const getlogin = async () => {
+        try {
+            const response = await axios.post('http://localhost:1133/auth/login', {
+                email: 'dominik.pokrzywa@gmail.com',
+                password: '123'
+            });
+
+            const token = response.data.token;
+            console.log('Token:', token);
+            setToken(token);
+            return token;
+
+        } catch (error) {
+            console.error('Błąd podczas logowania:', error.response ? error.response.data : error.message);
+        }
+    };
+
+    const getUser = async (token) => {
+        let config = {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        };
+        try {
+            const response = await axios.get('http://localhost:1133/user', config);
+            console.log('users ' + JSON.stringify(response.data, null, 2));
+            setUser(response.data[22]); 
+        } catch (error) {
+            console.error('Błąd podczas uzyskiwania użytkownika:', error.response ? error.response.data : error.message);
+        }
+    }
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const token = await getlogin();
+            if (token) {
+                await getUser(token);
+            }
+        };
+        fetchData();
+    }, []);
+
+
     return(<div className="navbar">
-        <a className="nav-button" href="/participants">Uczestnicy</a>
-        <a className="nav-button" href="/announcements">Ogłoszenia</a>
-        <a className="nav-button" href="/schedule">Harmonogram</a>
-        <a className="nav-button" href="/staff">Sztab</a>
-        <a className="nav-button" href="/settings">Ustawienia</a>
+        <a className="nav-button" href="/participants"> 
+        <img src={participants}/>
+        Uczestnicy 
+        </a>
+        <a className="nav-button" href="/announcements">
+        <img src={announcements}/>
+        Ogłoszenia</a>
+        <a className="nav-button" href="/schedule">
+        <img src={schedule}/>
+        Harmonogram</a>
+        <a className="nav-button" href="/staff">
+        <img src={staff}/>
+        Sztab</a>
+        <a className="nav-button" href="/settings">
+        <img src={setting}/>
+        Ustawienia</a>
+        <h3 className="navUser">{user.firstName} {user.lastName}</h3>
+       <h3 className="navEmail">{user.email} testEmail@test.pl</h3>   {/*w danych nie było mojego emaila na razie testuje na 1 osobie */}
+
+
     </div>)
 }
 
--- admin-panel/src/Components/schedule.js
+++ admin-panel/src/Components/schedule.js
@@ -68,31 +68,16 @@ function Schedule() {
     return (
         <div className="schedule">
             <h1>Harmonogram jest tutaj</h1>
-            <Table>
-            <div className="scheduleBG">
-                <div className="scheduleButtons">
-                <button className="dayButtonMid">
-Sobota
-        </button>
-                </div>
+            <Table className='scheduleTable'>
+
 
             {activities.map(activity => (
                         <TableRow key={activity.id}>
-                            {/* <TableCell>{activity.title}</TableCell>
+                            <TableCell>{activity.title}</TableCell>
                             <TableCell>{formatTime(activity.timeFrom)}</TableCell>
-                            <TableCell>{activity.description}</TableCell> */}
-                                 <div className="scheduleCard" >
-    <div className="scheduleTime" >{formatTime(activity.timeFrom)} - {formatTime(activity.timeTo)}</div>
-    <div className="scheduleContent" >{activity.title}</div>
-<button className="scheduleButton">
-<svg width="26" height="46" viewBox="0 0 26 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M25.1213 25.1213C26.2929 23.9497 26.2929 22.0503 25.1213 20.8787L6.02944 1.7868C4.85786 0.615223 2.95837 0.615223 1.7868 1.7868C0.615223 2.95837 0.615223 4.85786 1.7868 6.02944L18.7574 23L1.7868 39.9706C0.615223 41.1421 0.615223 43.0416 1.7868 44.2132C2.95837 45.3848 4.85786 45.3848 6.02944 44.2132L25.1213 25.1213ZM22 26H23V20H22V26Z" fill="#1D335C"/>
-</svg>
-</button>
-</div>
+                            <TableCell>{activity.description}</TableCell>
                         </TableRow>
                     ))}
-            </div>
                    
 
             </Table>
