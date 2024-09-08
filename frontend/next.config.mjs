8b17530a114248999d6de55aba65cc2a0f5c5bb3
Dominik Pokrzywa <dominik.pokrzywa@essyo.pl>
2024-09-08 23:40:31 +0200

    Before deployment changes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,6 +5,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreDevErrors: true,
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
