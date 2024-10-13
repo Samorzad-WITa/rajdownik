04ef3097488e9fb04444b5c48d644a53383ede27
Dominik Pokrzywa <dominik.pokrzywa@essyo.pl>
2024-10-13 09:45:02 +0200

    Small fix

--- backend/service/src/main/java/pl/pwr/ite/server/service/impl/UserServiceImpl.java
+++ backend/service/src/main/java/pl/pwr/ite/server/service/impl/UserServiceImpl.java
@@ -88,7 +88,7 @@ public class UserServiceImpl extends FilterableEntityServiceBase<User, UserFilte
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         var path = QUser.user;
-        var tuple = new JPAQuery<>(entityManager).select(path.id, path.email, path.type).from(path).where(path.email.eq(username)).fetchOne();
+        var tuple = new JPAQuery<>(entityManager).select(path.id, path.email, path.type).from(path).where(path.email.equalsIgnoreCase(username)).fetchOne();
         if(tuple == null) {
             throw new UsernameNotFoundException(String.format("User with email '%s' not found", username));
         }
